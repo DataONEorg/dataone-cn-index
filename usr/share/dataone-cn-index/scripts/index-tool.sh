7ef3967b0325c8ad6f794332b78763354511ec21
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-10-17 19:25:51 +0000

    small update to index tool script to stop/start index task generator - to ensure its running during rebuild/refresh.

--- usr/share/dataone-cn-index/scripts/index-tool.sh
+++ usr/share/dataone-cn-index/scripts/index-tool.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+/etc/init.d/d1-index-task-generator stop
+/etc/init.d/d1-index-task-generator start
 /etc/init.d/d1-index-task-processor stop
 java -jar /usr/share/dataone-cn-index/d1_index_build_tool.jar -a
 /etc/init.d/d1-index-task-processor start
-

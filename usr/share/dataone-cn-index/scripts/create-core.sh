3228cd293d4ba146b8ff726e7a2099d583c165a8
Robert Waltz <rwaltz@utk.edu>
2014-05-20 19:42:26 +0000

    refs #4470 merge from Jing's Branch

--- usr/share/dataone-cn-index/scripts/create-core.sh
+++ usr/share/dataone-cn-index/scripts/create-core.sh
@@ -9,11 +9,11 @@ else
 fi
 
 mkdir /var/lib/solr/data/$name
-chown tomcat6.tomcat6 /var/lib/solr/data/$name
+chown tomcat7:tomcat7 /var/lib/solr/data/$name
 
 mkdir -p /usr/share/solr/$name/conf
 
 cp -a /usr/share/dataone-cn-index/solr-core-template/* /usr/share/solr/$name/conf/
 sed -i "s/CORENAME/$name/" /usr/share/solr/$name/conf/solrconfig.xml
-chown -R tomcat6.tomcat6 /usr/share/solr/$name
+chown -R tomcat7:tomcat7 /usr/share/solr/$name
 curl "http://localhost:8080/solr/admin/cores?action=CREATE&name=$name&instanceDir=/usr/share/solr/$name"

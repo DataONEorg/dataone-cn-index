64e30c5cf75cec9fea0d5ac36d9bbe374be5cb56
Robert Waltz <rwaltz@utk.edu>
2015-10-17 01:47:56 +0000

    refs #7441
    move clearEventIndex.sh and clearSearchIndex.sh to dataone-cn-solr

--- usr/share/dataone-cn-index/scripts/clearSearchIndex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Delete all entries in the SOLR index
-XML="<delete><query>*:*</query></delete>"
-SOLR="http://localhost:8983/solr/search_core/update"
-
-#Delete
-curl "${SOLR}?commit=true" -H "Content-Type: text/xml" --data-binary ${XML}
-#Optimize
-curl "${SOLR}?optimize=true&waitFlush=true"

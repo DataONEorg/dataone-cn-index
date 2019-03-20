a7700732d925c277045f6ecddbead202ce893678
Jing Tao <tao@nceas.ucsb.edu>
2019-03-20 18:55:04 +0000

    Upgrade the version to 2.3.9. Change tomcat from 7 to 8 and postgresql from 9.3 to 10.

--- usr/share/dataone-cn-index/scripts/create-core.sh
+++ usr/share/dataone-cn-index/scripts/create-core.sh
@@ -11,9 +11,9 @@ else
 fi
 SOLR_URL="http://localhost:8983/solr"
 mkdir /var/lib/solr/data/$name
-chown tomcat7:tomcat7 /var/lib/solr/data/$name
+chown tomcat8:tomcat8 /var/lib/solr/data/$name
 mkdir -p /usr/share/solr/$name/conf
 cp -a /usr/share/dataone-cn-index/solr-core-template/* /usr/share/solr/$name/conf/
 sed -i "s/CORENAME/$name/" /usr/share/solr/$name/conf/solrconfig.xml
-chown -R tomcat7:tomcat7 /usr/share/solr/$name
+chown -R tomcat8:tomcat8 /usr/share/solr/$name
 curl "${SOLR_URL}/admin/cores?action=CREATE&name=$name&instanceDir=/usr/share/solr/$name"
